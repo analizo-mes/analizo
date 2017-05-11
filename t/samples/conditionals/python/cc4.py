f0a7cad1e484b4d7940fca129ae9adadb7904700
Izabela Cardoso <cizabelacristina@gmail.com>
2017-05-11 18:21:32 -0300

    Creating cyclomatic complexity tests for python

---
+++ t/samples/conditionals/python/cc4.py
@@ -0,0 +1,10 @@
+def cc4():
+  if something:
+    print "ok"
+  else:
+    if otherthing:
+      if someotherthing:
+        print "don't know"
+      else:
+        print "me neither"
+      printf "nope"
