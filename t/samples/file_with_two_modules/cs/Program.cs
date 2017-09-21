35dbe0a797252e730e8538a1a00a4c48cf539dbf
Kleber <kleberbritomoreira10@gmail.com>
2017-09-21 12:57:56 -0300

    Adding samples c# in file with two modules
    Signed-off-by: Kleber <kleberbritomoreira10@gmail.com>

---
+++ t/samples/file_with_two_modules/cs/Program.cs
@@ -0,0 +1,13 @@
+using classes;
+
+public class Program {
+  
+  static void Main(string[] args){
+    Class1 c1 = new Class1();
+    Class2 c2 = new Class2();
+    c1.say();
+    c2.say();	
+  }
+
+}
+
