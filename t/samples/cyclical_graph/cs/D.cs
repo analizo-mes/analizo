af0cc28b0722aaddc6c201e2c8ad6105d20b7ebe
Kleber <kleberbritomoreira10@gmail.com>
2017-09-22 09:45:55 -0300

    Adding samples for csharp in cyclical graph
    Signed-off-by: Kleber <kleberbritomoreira10@gmail.com>

---
+++ t/samples/cyclical_graph/cs/D.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace d {
+
+  public class D {
+    private string _name;
+  
+    public D(string name){
+      this._name = name;
+    }
+  
+    public string name(){
+    	return this._name;
+    } 
+  }
+} 
